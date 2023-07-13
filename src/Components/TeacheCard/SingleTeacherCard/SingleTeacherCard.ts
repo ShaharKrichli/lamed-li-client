085569fd9e88da0abdf4cf96cfc19d7915be7479
Shalom Copperman <shalomcop@gmail.com>
2023-07-13 18:59:49 +0300

    wip

---
+++ src/Components/TeacheCard/SingleTeacherCard/SingleTeacherCard.ts
@@ -0,0 +1,10 @@
+export interface SingleTeacherCard {
+    name: string
+    profession: string
+    primaryDesc: string
+    secondaryDesc: string
+    picture: string
+    age: string
+    academicInstitution: string
+    pricePerLesson: number
+}
---
+++ src/Components/TeacheCard/TeacherCard.data.ts
@@ -0,0 +1,14 @@
+import { SingleTeacherCard } from "./SingleTeacherCard/SingleTeacherCard"
+
+export const TEACHER_CARDS: SingleTeacherCard[] = [
+    {
+        name: '',
+        profession: '',
+        primaryDesc: '',
+        secondaryDesc:'',
+        picture:'',
+        age: '',
+        academicInstitution:'',
+        pricePerLesson:0
+    }
+]
