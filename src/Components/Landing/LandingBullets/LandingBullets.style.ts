0c479eece2cdf57cd4df22b72ffdfead3c63b2e0
amitzalts <amitzalts@gmail.com>
2023-07-24 00:39:52 +0300

    added 2 break points to the landing bullets comp to accomplish responisvness

--- src/Components/Landing/LandingBullets/LandingBullets.style.ts
+++ src/Components/Landing/LandingBullets/LandingBullets.style.ts
@@ -1,11 +1,51 @@
 import { styled } from "@mui/material/styles";
+import { margin } from "@mui/system";
+
+// export const container = styled("div")({
+//   backgroundColor: "#f1fdf7",
+//   display: "flex",
+//   alignItems: "center",
+//   justifyContent: 'space-around',
+//   paddingBlock: '3rem'
+// });
+
+// export const title = styled("div")({
+//   fontSize: "2rem",
+//   paddingBottom: '1.5rem',
+// });
+
+// export const AllBulletWrapper = styled("div")({
+//   width: "40rem",
+// });
+
+// export const CenterLeftImg = styled("img")({
+//   width: "35rem",
+//   borderRadius: "20px",
+// });
+
+
+// breakpoints for half screen and mobile
+const halfScreenBreakpoint = "900px";
+const mobileBreakpoint = "600px";
+
 
 export const container = styled("div")({
   backgroundColor: "#f1fdf7",
   display: "flex",
   alignItems: "center",
   justifyContent: 'space-around',
-  paddingBlock: '3rem'
+  paddingBlock: '3rem',
+
+  // Media query for half screen screens
+  [`@media (max-width: ${halfScreenBreakpoint})`]: {
+    flexDirection: "column",
+  },
+
+  // Media query for mobile
+  [`@media (max-width: ${mobileBreakpoint})`]: {
+    flexDirection: "column",
+    paddingBlock: '1rem',
+  },
 });
 
 export const title = styled("div")({
@@ -15,9 +55,32 @@ export const title = styled("div")({
 
 export const AllBulletWrapper = styled("div")({
   width: "40rem",
+  display: "flex",
+  flexDirection:"column",
+
+  // Media query for half screen screens
+  [`@media (max-width: ${halfScreenBreakpoint})`]: {
+    width: "30rem",
+  },
+
+  // Media query for mobile
+  [`@media (max-width: ${mobileBreakpoint})`]: {
+    width: "25rem",
+  },
 });
 
 export const CenterLeftImg = styled("img")({
   width: "35rem",
   borderRadius: "20px",
+
+  // Media query for half screen screens
+  [`@media (max-width: ${halfScreenBreakpoint})`]: {
+    width: "30rem",
+  },
+
+  // Media query for mobile
+  [`@media (max-width: ${mobileBreakpoint})`]: {
+    width: "25rem",
+  },
 });
+
