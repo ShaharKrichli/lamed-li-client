a94abe468b85a5b349128f64b629c6baaba90c6b
amitzalts <amitzalts@gmail.com>
2023-07-29 21:31:13 +0300

    added use effect to handle the conditions of the tokens required

--- src/context/authState.tsx
+++ src/context/authState.tsx
@@ -1,7 +1,10 @@
-import { createContext, useContext, useState, FC } from 'react';
+import { createContext, useContext, useState, FC, useEffect } from 'react';
 import { AuthState, IAuthStateContext } from './IAuthState';
 import { LOCAL_STORAGE_NAMES } from '../consts/login';
 import { getTokenFromStorage } from '../utils/auth/auth';
+
+import axios from "axios";
+import { ITokens } from '../types/interfaces/IToken';
 
 const AuthStateContext = createContext<IAuthStateContext>({} as IAuthStateContext);
 
@@ -11,12 +14,42 @@ export const AuthStateProvider: FC<{ children: React.ReactNode; }> = ({ children
 
     const [authState, setAuthState] = useState<number>(AuthState.InProgress); //initial state
 
-    let authToken = getTokenFromStorage(LOCAL_STORAGE_NAMES.AUTH); //the func returns: json.parse, undefiende/false
-
-    if (!authToken) {
-        setAuthState(AuthState.unAuthenticated)
-    } else setAuthState(AuthState.Authenticated)
-
+    useEffect(() => {
+        (async () => {
+            try {
+                const authTokenFromLocalStorage = localStorage.getItem(LOCAL_STORAGE_NAMES.AUTH);
+                // Check if token exists and not revoked
+                if (!authTokenFromLocalStorage) throw new Error("auth token on local storage not found");
+                let authToken = getTokenFromStorage(authTokenFromLocalStorage);
+                // If exists and not revoked, set authState AuthState.Authenticated
+                if (!authToken) {
+                    // If doesn't exist or not valid, set authState AuthState.unAuthenticated
+                    setAuthState(AuthState.unAuthenticated);
+                    // If exists else check for a refresh token
+                    const refreshToken = localStorage.getItem(LOCAL_STORAGE_NAMES.REFRESH_TOKEN);
+                    if (refreshToken) {
+                        const { data } = await axios.post<ITokens>(
+                            `https://localhost:3000/login/refresh-token`,
+                            {},
+                            {
+                                headers: {
+                                    Authorization: `Bearer ${refreshToken}`
+                                }
+                            }
+                        );
+                        // If exists and not revoked, call the refresh token API and set new tokens
+                        localStorage.setItem(LOCAL_STORAGE_NAMES.AUTH, data.accessToken);
+                        localStorage.setItem(LOCAL_STORAGE_NAMES.REFRESH_TOKEN, data.refreshToken);
+                        authToken = data.accessToken;
+                    }
+                } else {
+                    setAuthState(AuthState.Authenticated);
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        })();
+    }, []);
 
     const value: IAuthStateContext = { authState, setAuthState }
 
