a1f50687f68a3608b3d75588ca5e821a4c2792c5
amitzalts <amitzalts@gmail.com>
2023-07-30 20:33:16 +0300

    Update authState.tsx

--- src/context/authState.tsx
+++ src/context/authState.tsx
@@ -17,16 +17,13 @@ export const AuthStateProvider: FC<{ children: React.ReactNode; }> = ({ children
     useEffect(() => {
         (async () => {
             try {
-                const authTokenFromLocalStorage = localStorage.getItem(LOCAL_STORAGE_NAMES.AUTH);
                 // Check if token exists and not revoked
-                if (!authTokenFromLocalStorage) throw new Error("auth token on local storage not found");
-                let authToken = getTokenFromStorage(authTokenFromLocalStorage);
+                let authToken = getTokenFromStorage(LOCAL_STORAGE_NAMES.AUTH);
                 // If exists and not revoked, set authState AuthState.Authenticated
                 if (!authToken) {
                     // If doesn't exist or not valid, set authState AuthState.unAuthenticated
-                    setAuthState(AuthState.unAuthenticated);
                     // If exists else check for a refresh token
-                    const refreshToken = localStorage.getItem(LOCAL_STORAGE_NAMES.REFRESH_TOKEN);
+                    const refreshToken = getTokenFromStorage(LOCAL_STORAGE_NAMES.REFRESH_TOKEN);
                     if (refreshToken) {
                         const { data } = await axios.post<ITokens>(
                             `https://localhost:3000/login/refresh-token`,
@@ -40,11 +37,11 @@ export const AuthStateProvider: FC<{ children: React.ReactNode; }> = ({ children
                         // If exists and not revoked, call the refresh token API and set new tokens
                         localStorage.setItem(LOCAL_STORAGE_NAMES.AUTH, data.accessToken);
                         localStorage.setItem(LOCAL_STORAGE_NAMES.REFRESH_TOKEN, data.refreshToken);
-                        authToken = data.accessToken;
+                        setAuthState(AuthState.Authenticated)
+                        return
                     }
-                } else {
-                    setAuthState(AuthState.Authenticated);
                 }
+                setAuthState(AuthState.unAuthenticated);
             } catch (error) {
                 console.error(error);
             }
